662ce2ab89fa2effe3e476aa97fdd0fff3e34639
asour <52638772+aMySour@users.noreply.github.com>
2024-08-04 18:34:03 -0300

    traits

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This crate contains Rust bindings to **Box2D v3.0** (`box2c`).
 
 We are using [our fork of `box2c`](https://github.com/Carroted/box2c), which makes only one change: increasing the max translation/rotation settings, which are hardcoded into `box2c`.
 
+Lastly, this crate implements certain traits for `box2c` types, such as `Hash`, `PartialEq`, etc. We add these whenever they are needed for our projects which use this crate.
+
 ## License
 
 MIT or Apache2 at your option, but note that `box2c` itself is licensed under MIT
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,24 @@
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)] // just for now since we get literally over 300 warnings about improper c type with u128 rn
 
+use std::hash::{Hash, Hasher};
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+impl Hash for b2BodyId {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.index1.hash(state);
+        self.world0.hash(state);
+        self.revision.hash(state);
+    }
+}
+
+impl PartialEq for b2BodyId {
+    fn eq(&self, other: &Self) -> bool {
+        self.index1 == other.index1
+            && self.world0 == other.world0
+            && self.revision == other.revision
+    }
+}
+
+impl Eq for b2BodyId {}
